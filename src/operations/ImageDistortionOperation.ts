52e96043ba1a6805b61a1df330b1389417350857
Ilya <midian.ip@gmail.com>
2024-04-23 22:47:23 +0300

    ImageDistortionOperation

--- src/operations/ImageDistortionOperation.ts
+++ src/operations/ImageDistortionOperation.ts
@@ -1,6 +1,5 @@
 import { Telegraf, Context } from "telegraf";
 import { Operation } from "./Operation.js";
-import { Magick } from 'magickwand.js';
 import fetch from 'node-fetch';
 
 type TelegramPhoto = {
@@ -9,35 +8,81 @@ type TelegramPhoto = {
 }
 
 export class ImageDistortionOperation implements Operation {
-
+    imageManipulationServiceUrl: string;
+    constructor() {
+        this.imageManipulationServiceUrl = process.env.IMAGE_MANIPULATION_SERVICE_URL;
+    }
     async register(bot: Telegraf<Context>): Promise<void> {
         bot.hears(/нука|Нука|жмыхни|Жмыхни/, async (ctx, next) => {
-            const imageIds : TelegramPhoto[] = ctx.update.message.reply_to_message["photo"];
-            if (!imageIds){
+            const file = this.getFile(ctx.update.message?.reply_to_message);
+            if (!file){
                 return next();
             }
 
-            const bestQuality =
-                imageIds.reduce((prev, current) => prev.file_size > current.file_size ? prev : current)
-
-            const fileMeta = await fetch(`https://api.telegram.org/bot${ctx.telegram.token}/getFile?file_id=${bestQuality.file_id}`)
+            const fileMeta = await fetch(`https://api.telegram.org/bot${ctx.telegram.token}/getFile?file_id=${file.fileId}`)
                 .then(result => result.json());
 
             // @ts-ignore
             const binary = await fetch(`https://api.telegram.org/file/bot${ctx.telegram.token}/${fileMeta.result.file_path}`)
-                .then( result => result.blob());
+                .then( result => result.arrayBuffer());
 
-            const blob = new Magick.Blob(await binary.arrayBuffer());
-            const image = new Magick.Image;
-            await image.readAsync(blob);
-            const originalSize = image.size();
-            await image.liquidRescaleAsync('40x40%!');
-            await image.resizeAsync(originalSize);
-            await image.writeAsync(blob);
+            let url: string;
+            if (file.type === 'image'){
+                url = 'image/distort-image';
+            }
+            if (file.type === 'video' || file.type == 'videoNote'){
+                url = 'image/distort-video';
+            }
+            const t = JSON.stringify({ base64: Buffer.from(binary).toString('base64')});
+            const result = await fetch(`${this.imageManipulationServiceUrl}/${url}`, {
+                method: 'POST',
+                body: JSON.stringify({ base64: Buffer.from(binary).toString('base64')}),
+                headers: { 'Content-Type': 'application/json' }
+            }).then( res => res.json())
 
-            const replyImageBuffer = Buffer.from( await blob.base64Async(), 'base64');
+            // @ts-ignore
+            if (!result.base64){
+                return next();
+            }
 
-            await ctx.replyWithPhoto({ source: replyImageBuffer }, { reply_parameters: { message_id: ctx.message.message_id} })
+            // @ts-ignore
+            const imgbase = Buffer.from(result.base64, 'base64');
+
+            if (file.type === 'image'){
+                // @ts-ignore
+                await ctx.replyWithPhoto({ source: imgbase }, { reply_to_message_id: ctx.message.message_id })
+            }
+            if (file.type === 'video' || file.type == 'videoNote'){
+                // @ts-ignore
+                await ctx.replyWithAnimation({ source: imgbase }, { reply_to_message_id: ctx.message.message_id })
+            }
         });
+    }
+
+    private getFile(replyToMessage: any): {fileId: string, type: 'video' | 'image' | 'videoNote'} {
+        if (!replyToMessage){
+            return undefined;
+        }
+
+        if (replyToMessage.photo){
+            const imageIds = replyToMessage.photo;
+            const mostQuality =  imageIds.reduce(
+                (prev, current) => {
+                    return prev.file_size > current.file_size ? prev : current
+                }
+            )
+            return { fileId: mostQuality.file_id, type: 'image' }
+
+        }
+
+        if (replyToMessage.animation){
+            return { fileId: replyToMessage.animation.file_id, type: 'video' }
+        }
+
+        if (replyToMessage.video_note){
+            return { fileId: replyToMessage.video_note.file_id, type: 'video' }
+        }
+
+        return undefined;
     }
 }
